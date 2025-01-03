55a0a9ca54d621eab99d13a4bb0f6832509ef2d5
Usama <usamarazaaq3@gmail.com>
2025-01-03 12:11:58 +0500

    modified Link component in single student page

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -144,7 +144,7 @@ const SingleStudentPage = () => {
             </Link>
             <Link
               className="p-3 rounded-md bg-lamaYellowLight"
-              href={`/list/results?classId=${2}`}
+              href={`/list/results?studentId=${"student2"}`}
             >
               Student&apos;s Results
             </Link>
