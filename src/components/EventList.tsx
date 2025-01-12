83dda75bfe0b1f706d10e429431eb5bd54187562
Usama <usamarazaaq3@gmail.com>
2025-01-12 12:08:45 +0500

    The EventList access dateParam which contains date as a string then it sets starting and ending time on this new Date modified in date variable and inside query fetches the events matching to this time

---
+++ src/components/EventList.tsx
@@ -0,0 +1,40 @@
+import prisma from "@/lib/prisma";
+
+const EventList = async ({ dateParam }: { dateParam: string | undefined }) => {
+
+  // dateParam is a string that can represent a specific date.
+  // If dateParam is undefined, it defaults to the current date (new Date()).  
+  const date = dateParam ? new Date(dateParam) : new Date();
+
+// gte: Greater than or equal to the start of the day.
+// lte: Less than or equal to the end of the day.
+  const eventsData = await prisma.event.findMany({
+    where: {
+      startTime: {
+        gte: new Date(date.setHours(0, 0, 0, 0)),
+        lte: new Date(date.setHours(23, 59, 59, 999)),
+      },
+    },
+  });
+
+  return eventsData.map((event) => (
+    <div
+      className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple"
+      key={event.id}
+    >
+      <div className="flex items-center justify-between">
+        <h1 className="font-semibold text-gray-600">{event.title}</h1>
+        <span className="text-gray-300 text-xs">
+          {event.startTime.toLocaleTimeString("en-UK", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          })}
+        </span>
+      </div>
+      <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+    </div>
+  ));
+};
+
+export default EventList;
